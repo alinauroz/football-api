d4ef9669c18311175b4dcace5e840ba1003395c3
Ali Nauroze <alinauroze@hotmail.com>
2021-02-23 23:32:06 +0500

    app initialized

---
+++ client/App.style.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+import {Dimenions} from 'react-native'
+
+module.exports = {
+    container = {
+        width: Dimenions.get('window').width - 20,
+        marginLeft: 10,
+    }
+}
---
+++ client/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+import {
+    Text,
+    View
+} from 'react-native'
+import { Button } from 'react-native-elements';
+
+const Login = (props) => {
+    return (
+        <Button
+          title="Solid Button"
+        />
+    )
+}
+
+export default Login;
