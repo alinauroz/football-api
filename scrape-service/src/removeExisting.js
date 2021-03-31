eba04cf6261f113797a677c13b221e63d8113386
Ali Nauroze <alinauroze@hotmail.com>
2021-03-31 23:14:51 +0500

    remove existing made async

--- scrape-service/src/removeExisting.js
+++ scrape-service/src/removeExisting.js
@@ -3,26 +3,31 @@ const util = require('util');
 
 const readFile = util.promisify(fs.readFile);
 
-const removeExisting = (urls) => {
-    let existingUrls = [];
-    
-    readFile('./src/urls.log', 'utf-8').then((data) => {
+const removeExisting = async (urls) => {
+    try {
+        let existingUrls = [];
+        
+        let data = await readFile('./src/urls.log', 'utf-8')
         existingUrls = data.split("\n");
-    }).catch (err => {
-        console.warn('No log file found')
-    });
-
-    if (existingUrls) {
-        let existingIndex = [];
-        urls.forEach((url, index) => {
-            if (existingUrls.indexOf(url) > -1) {
-                existingIndex.push(index);
-            }
-        });
-        existingIndex.reverse().forEach((urlIndex) => {
-            urls.splice(urlIndex, 1)
-        });
+        if (existingUrls) {
+            let existingIndex = [];
+            urls.forEach((url, index) => {
+                if (existingUrls.indexOf(url) > -1) {
+                    existingIndex.push(index);
+                }
+            });
+            existingIndex.reverse().forEach((urlIndex) => {
+                urls.splice(urlIndex, 1)
+            });
+        }
     }
+    catch (err) {
+        console.log('URL log file not found');
+    }
+    
 }
+
+let a = ["http://google.com/", "abc.cp"]
+removeExisting(a).then(() => console.log(a))
 
 module.exports = removeExisting;
