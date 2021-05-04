318b414044d7517a02ea4d1ae4d4c18c3123d537
Ali Nauroze <alinauroze@hotmail.com>
2021-05-05 00:13:58 +0500

    match summary added

--- client/components/Matches/Match.Detail.js
+++ client/components/Matches/Match.Detail.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { Card } from 'react-native-elements';
 import YoutubePlayer from 'react-native-youtube-iframe';
+import moment from "moment";
+import { getTeamById } from "../../utils/getTeams"
 import styles from './Matches.Style';
 
 
@@ -23,6 +25,31 @@ export default function ({ match }) {
                     <Text>{ match.isLive ? "Live Streaming" : "Highlights" }</Text>
                 </Card>: null
             }
+            <Card>
+                <Text style={{marginBottom: 10}}>{match.summary.length > 0 ? "Match Summary" : "No Match Summary"}</Text>
+                {match.summary.length > 0 ? <Card.Divider/> : null}
+                {
+                    match.summary.map((event) => {
+                        let time = moment(event.time).format("hh:mm A");
+                        let str;
+                        if (event.action === "goal") {
+                            str = `Goal by ${getTeamById(event.team)?.name}`
+                        }
+                        else if (event.action === "foul") {
+                            str = `${getTeamById(event.team)?.name} foul`
+                        }
+                        else if (event.action === "break") {
+                            str = `Match Break`
+                        }
+                        return (
+                            <View style={{ display: 'flex', flexDirection: 'row', marginTop: 3 }}>
+                                <Text style={{marginRight: 10, fontWeight: 'bold', color: '#333'}}>{time}</Text>
+                                <Text style={{fontWeight: 'bold', color: '#333'}}>{str}</Text>
+                            </View>
+                        );
+                    })
+                }
+            </Card>
         </ScrollView>
     );
 }
